5d2c5f32714a45ef8768614860b13eee2a39d831
joshua-murphy <josh.murphy11@gmail.com>
2017-11-15 20:01:42 -0700

    add randomization

--- main.js
+++ main.js
@@ -1,27 +1,66 @@
+var sampleArr = ["mercury.jpg", "venus.jpg", "earth.jpg", "mars.jpg", "jupiter.jpg", 
+"saturn.png", "uranus.jpg", "neptune.jpg", "mercury.jpg", "venus.jpg", "earth.jpg", 
+"mars.jpg", "jupiter.jpg", "saturn.png", "uranus.jpg", "neptune.jpg"]
+var randArr = []
+countUp = 0
+
+
+function getRandNum() {
+  var rand = Math.floor(Math.random() * 16 )
+  var aarVar = sampleArr[rand]
+  if (aarVar === undefined)
+    getRandNum()
+    else
+      randArr.push(aarVar)
+      delete sampleArr[rand]
+}
+
+while (randArr.length < 16) {
+  getRandNum()
+  changeAlt(countUp)
+  ++countUp
+}
+
+console.log(randArr)
+
+function changeAlt(num) {
+  var numVar = ($("div").children("div").children("img:eq(" + num + ")"))
+  console.log(numVar)
+  if( numVar.attr("src") == num ) {
+    console.log("yay")
+    numVar.attr("alt", "images/" + randArr[num])
+  } else
+  console.log("id check fail")
+}
+
 $("img").attr("src", "images/card.jpg")
+
 
 $(document).ready( function() {
 
   var ansArr = []  
   var successArr = []
 
+
+
+
+
   $("img").on("click", function() {
     if( $(this).attr("id") === "no_click") {
       return
   } else {
-      console.log($(this).attr('id'))    
+      // console.log($(this).attr("id"))    
       var source = $(this).attr("alt")
-      $(this).attr("src", "images/" + source )
+      $(this).attr("src", source )
       $(this).attr("id", "no_click") 
       addToArr(source) }
   })
 
   function addToArr(source) {  
     $(this).attr("id", "no_click")
-    ansArr.push("images/" + source)
+    ansArr.push(source)
     ansArr.length > 1 ? checkMatch() : console.log("")
   }
-
   
 
   function checkMatch() {
@@ -32,19 +71,20 @@ $(document).ready( function() {
 
   function flipOver() {
     $("img").attr("id", "no_click")    
-    console.log("no click")
+    // console.log("no click")
     setTimeout( function() {
       $("img").attr("src", "images/card.jpg" )     
       ansArr = []
-      console.log("waiting")
+      // console.log("waiting")
       $("img").attr("id", "")
     }, 500)
   }
 
   function success() {
-    console.log("success")
+    // console.log("success")
     successArr.push(ansArr[1])
-    console.log(successArr)
+    // console.log(successArr)
   }
+
 
 })
